4f9be633065d17509f125e631b50f8ebaf9de1e1
Brennan Gilbert <github@brennangilbert.com>
2016-09-11 14:57:15 -0600

    Styling for responsive (menu, etc)

--- static/js/header.js
+++ static/js/header.js
@@ -5,7 +5,7 @@
 function headerAnimation() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 200
+            shrinkOn = 160
         if ($( "body" ).hasClass("home")) {
             if (distanceY > shrinkOn) {
                 $( "header" ).addClass( "normal" );
@@ -14,5 +14,23 @@ function headerAnimation() {
             }
         }
     });
-}
-window.onload = headerAnimation();
+};
+
+
+// Mobile Menu
+
+function headerMenu() {
+    $("#menu-button").click(function(){
+        $("body > header").toggleClass("show");
+        $("#header-menu").toggleClass("show");
+        // $(this).removeClass("show");
+    });
+    // $("#menu-close").click(function(){
+    //     $("body > header").removeClass("show");
+    //     $("#header-menu").removeClass("show");
+    //     $("#menu-button").addClass("show");
+    // });
+};
+
+
+window.onload = headerAnimation(), headerMenu();
