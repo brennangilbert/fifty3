9b699e0b0d1b1c8221bfc85ad06adc505e82a8a7
Brennan Gilbert <github@brennangilbert.com>
2016-10-24 19:41:03 -0600

    Fix php tags

--- wp/wp-content/themes/fifty3/archive-work.php
+++ wp/wp-content/themes/fifty3/archive-work.php
@@ -12,11 +12,11 @@ get_template_part( 'template-parts/banner' ); ?>
 
 <section class="video-bg">
 	<video id="auto-video" autoplay loop muted style="background-image: url('<? bloginfo('template_directory'); ?>/img/fifty3-work-video-placeholder.jpg');">
-		<source src="<? bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.webm" type="application/webm">
-		<source src="<? bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.mp4" type="video/mp4">
-		<source src="<? bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.ogv" type="video/ogv">
+		<source src="<?php bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.webm" type="application/webm">
+		<source src="<?php bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.mp4" type="video/mp4">
+		<source src="<?php bloginfo('template_directory'); ?>/video/fifty3_in_53_seconds-no_audio.ogv" type="video/ogv">
 	</video>
-	<img src="<? bloginfo('template_directory'); ?>/img/fifty3-work-video-placeholder.jpg">
+	<img src="<?php bloginfo('template_directory'); ?>/img/fifty3-work-video-placeholder.jpg">
 	<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script>
 	<div id="wistia-container" class="wistia_embed wistia_async_ntj3nh0g96 popover=true popoverContent=html popoverPreventScroll=true"
 	style="display:inline-block; white-space:nowrap;">
@@ -27,7 +27,7 @@ get_template_part( 'template-parts/banner' ); ?>
 	<div class="wrapper">
 		<nav class="filter">
 			<button class="active btn fil-cat" data-rel="all">All</button>
-			<?
+			<?php
 			// Exclude Uncategorized and Featured based on Local vs Live
 			if ( stristr( $_SERVER['SERVER_NAME'], 'localhost' ) ) {
 				$exclusions = '1 9';
@@ -48,7 +48,7 @@ get_template_part( 'template-parts/banner' ); ?>
 			endif;
 			?>
 		</nav>
-		<? 
+		<?php 
 		// Get Projects
 		$args = array('post_type' => 'work');
 		$loop = new WP_Query( $args );
@@ -68,15 +68,15 @@ get_template_part( 'template-parts/banner' ); ?>
 				}
 
 			?>
-				<a class="grid-1-3 square scale-anm all <? echo $cats ?>" href="<? echo get_permalink(); ?>"><div class="text"><span><? echo the_title(); ?></span></div><? echo get_the_post_thumbnail( $post_id, 'thumbnail', array( 'class' => 'bg' ) ); ?></a>
-			<? endwhile;
+				<a class="grid-1-3 square scale-anm all <?php echo $cats ?>" href="<?php echo get_permalink(); ?>"><div class="text"><span><?php echo the_title(); ?></span></div><?php echo get_the_post_thumbnail( $post_id, 'thumbnail', array( 'class' => 'bg' ) ); ?></a>
+			<?php endwhile;
 
 		echo '</div>';
 		?>
-		<a class="btn" href="<? echo get_page_link(11); ?>">Meet <strong>the Brains</strong> Behind the Ideas</a>
+		<a class="btn" href="<?php echo get_page_link(11); ?>">Meet <strong>the Brains</strong> Behind the Ideas</a>
 	</div>
 </section><!-- // .portfolio -->
 
 <!--// endwhile; -->
 
-<? get_footer(); ?>
+<?php get_footer(); ?>
