1be7ef6f081310b01ec711d3d85172139f6d7aae
vossavant <ryan@gospotcheck.com>
2016-10-29 13:04:10 -0600

    Update randomizer to only show each item once

--- wp/wp-content/themes/fifty3/js/slideshow.js
+++ wp/wp-content/themes/fifty3/js/slideshow.js
@@ -1,35 +1,8 @@
-// $("#slideshow > div:gt(0)").hide();
-
-var uniqueRandoms = [];
-var numRandoms = 5;
-function makeUniqueRandom() {
-    // refill the array if needed
-    if (!uniqueRandoms.length) {
-        for (var i = 0; i < numRandoms; i++) {
-            uniqueRandoms.push(i);
-        }
-    }
-    var index = Math.floor(Math.random() * uniqueRandoms.length);
-    var val = uniqueRandoms[index];
-
-    // now remove that value from the array
-    uniqueRandoms.splice(index, 1);
-
-    return val;
-}
-
+// Randomly show reasons
 setInterval(function() {
 	$("#slideshow .reason").fadeOut(1000);
-	var divs = $("#slideshow .reason").get().sort(function(){ 
+	var divs = $("#slideshow .reason.not-shown").get().sort(function(){
 		return Math.round(Math.random())-0.5; //so we get the right +/- combo
 	}).slice(0,1);
 	$(divs).fadeIn(1000).removeClass("not-shown");
-	// $(divs).appendTo(divs[0].parentNode).show();
-	// .next()
-	// .end()
-	// .appendTo('#slideshow');
 }, 4000);
-// $("#slideshow .reason").addClass('color-' + (makeUniqueRandom() + 1));
-
-
-// $(divs).appendTo(divs[0].parentNode).show();
